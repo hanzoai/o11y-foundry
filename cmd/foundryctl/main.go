e9cf7132deb1f3b84982bc17ca2e4a0760acdd70
Pandey <vibhupandey28@gmail.com>
2025-11-26 19:02:06 +0530

    feat(cmd): add subcommands for foundryctl (#6)
    #### Features
    
    - add subcommands for foundryctl

--- cmd/foundryctl/main.go
+++ cmd/foundryctl/main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
+	"log/slog"
+	"os"
+
+	"github.com/SigNoz/foundry/internal/instrumentation"
 	"github.com/spf13/cobra"
 )
 
-var _ = &cobra.Command{
-	Use: "foundryctl",
+func main() {
+
+	rootCmd := &cobra.Command{
+		Use:          "foundryctl",
+		SilenceUsage: true,
+		CompletionOptions: cobra.CompletionOptions{
+			DisableDefaultCmd: true,
+		},
+	}
+
+	// Register configuration.
+	cfg.RegisterFlags(rootCmd)
+
+	// Initialize instrumentation for the cmd/ package.
+	logger := instrumentation.NewLogger(false)
+
+	// Register commands.
+	registerGaugeCmd(rootCmd)
+	registerForgeCmd(rootCmd)
+	registerCastCmd(rootCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		logger.ErrorContext(rootCmd.Context(), "failed to execute command", slog.Any("error", err))
+		os.Exit(1)
+	}
 }
