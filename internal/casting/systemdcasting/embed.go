9d6dc948cfe6fe519af5ad314d271de99e097586
Nageshbansal <76246968+Nageshbansal@users.noreply.github.com>
2026-01-28 14:28:02 +0530

    fix(casting/systemd): handle binaries path and use otel-collector for migrations (#43)
    #### Fixes
    
    - Run migrations with new signoz-otel-collector not with deprecated
    schema-migrator
    - Handle binaries path better with the flexibility to allow custom
    binaries path
    
    related: https://github.com/SigNoz/platform-pod/issues/1594

--- internal/casting/systemdcasting/embed.go
+++ internal/casting/systemdcasting/embed.go
@@ -10,9 +10,10 @@ import (
 var templates embed.FS
 
 var (
-	signozServiceTemplate          *types.Template = types.MustNewTemplateFromFS(templates, "templates/signoz.service.gotmpl", types.FormatINI)
-	ingesterServiceTemplate        *types.Template = types.MustNewTemplateFromFS(templates, "templates/ingester.service.gotmpl", types.FormatINI)
-	telemetryStoreServiceTemplate  *types.Template = types.MustNewTemplateFromFS(templates, "templates/clickhouse.telemetrystore.v2556.service.gotmpl", types.FormatINI)
-	telemetryKeeperServiceTemplate *types.Template = types.MustNewTemplateFromFS(templates, "templates/clickhousekeeper.telemetrykeeper.v2556.service.gotmpl", types.FormatINI)
-	metaStoreServiceTemplate       *types.Template = types.MustNewTemplateFromFS(templates, "templates/postgres.metastore.service.gotmpl", types.FormatINI)
+	signozServiceTemplate                 *types.Template = types.MustNewTemplateFromFS(templates, "templates/signoz.service.gotmpl", types.FormatINI)
+	ingesterServiceTemplate               *types.Template = types.MustNewTemplateFromFS(templates, "templates/ingester.service.gotmpl", types.FormatINI)
+	telemetryStoreServiceTemplate         *types.Template = types.MustNewTemplateFromFS(templates, "templates/clickhouse.telemetrystore.v2556.service.gotmpl", types.FormatINI)
+	telemetryKeeperServiceTemplate        *types.Template = types.MustNewTemplateFromFS(templates, "templates/clickhousekeeper.telemetrykeeper.v2556.service.gotmpl", types.FormatINI)
+	metaStoreServiceTemplate              *types.Template = types.MustNewTemplateFromFS(templates, "templates/postgres.metastore.service.gotmpl", types.FormatINI)
+	telemetryStoreMigratorServiceTemplate *types.Template = types.MustNewTemplateFromFS(templates, "templates/migrator.telemetrystore.service.gotmpl", types.FormatINI)
 )
