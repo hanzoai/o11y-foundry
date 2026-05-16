2606307d38f37b23cba0cc7b59ef8aa35f527b0d
Nageshbansal <76246968+Nageshbansal@users.noreply.github.com>
2026-05-16 19:39:35 +0530

    feat: introduce collection agent schema (#117)
    #### Features
    
    - Adds collectionAgent Casting Kind
    
    Related: https://github.com/SigNoz/platform-pod/issues/1970

--- internal/foundry/cast.go
+++ internal/foundry/cast.go
@@ -6,6 +6,7 @@ import (
 	"log/slog"
 
 	"github.com/signoz/foundry/api/v1alpha1"
+	"github.com/signoz/foundry/api/v1alpha1/collectionagent"
 	"github.com/signoz/foundry/api/v1alpha1/installation"
 )
 
@@ -13,8 +14,16 @@ func (foundry *Foundry) Cast(ctx context.Context, machinery v1alpha1.Machinery, 
 	switch c := machinery.(type) {
 	case *installation.Casting:
 		return foundry.castInstallation(ctx, *c, poursPath)
+	case *collectionagent.Casting:
+		return foundry.castCollectionAgent(ctx, *c, poursPath)
 	}
 	return fmt.Errorf("unsupported casting kind %q", machinery.Kind())
+}
+
+func (foundry *Foundry) castCollectionAgent(ctx context.Context, config collectionagent.Casting, _ string) error {
+	foundry.Logger.WarnContext(ctx, "collectionagent cast not yet implemented; skipping",
+		slog.String("casting.metadata.name", config.Metadata.Name))
+	return nil
 }
 
 func (foundry *Foundry) castInstallation(ctx context.Context, config installation.Casting, poursPath string) error {
